80aaad2694e3c13ddc9b6163fe8feb5d4ac6cfa2
Jakhongir <jakhongiralikhanov@gmail.com>
2021-11-16 22:05:40 +0500

    staticfiles added

--- src/UTS/settings/base.py
+++ src/UTS/settings/base.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = 'django-insecure-2jh9!1ja8dz8en+kpk91c3q_(&576wi3=39$y$3%sqkv)k0g=v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
---
+++ src/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/user/own_proj/uts_docker/src/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
